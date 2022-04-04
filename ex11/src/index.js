f8dc5df59f2fb363d3e30ec2eaa9940bfd9423ac
Emir Dedic <dedic.emir17@gmail.com>
2022-04-04 17:12:51 +0200

    Zavrseno

---
+++ ex11/src/index.js
@@ -0,0 +1,11 @@
+//Only change code below this line
+function convertToInt(str){
+    return parseInt(str, 2);
+}
+
+convertToInt("BA");
+convertToInt("F1");
+convertToInt("JeffBezos");
+
+//Only change code below this line
+module.exports = convertToInt;
